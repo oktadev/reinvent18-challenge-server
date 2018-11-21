bcc435b1109f8699dee669c207beeaca8133341f
Randall Degges <r@rdegges.com>
2018-11-20 21:15:34 -0800

    Defining a model to store our challenge winner data.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   operatorAliases: false
 });
 
+const Winner = sequelize.define("winner", {
+  email: { type: Sequelize.TEXT }
+});
+
+Winner.sync({ force: true });
+
 app.use(async ctx => {
   ctx.body = "Hello, world.";
 });
