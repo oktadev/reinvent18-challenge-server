45719571f34090433f60990d387e7c453c8a0342
Randall Degges <r@rdegges.com>
2018-11-20 21:03:35 -0800

    Adding some basic Koa code.

---
+++ src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const Koa = require("koa");
+const Sequelize = require("sequelize");
+
+const app = new Koa();
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+app.use(async ctx => {
+  ctx.body = "Hello, world.";
+});
+
+app.listen(process.env.PORT);
