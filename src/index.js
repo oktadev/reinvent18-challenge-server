d7a8bc72bae053560836ac4fdc52ed9ea8e16df9
Randall Degges <r@rdegges.com>
2018-11-21 00:33:25 -0800

    Testing.

--- src/index.js
+++ src/index.js
@@ -55,11 +55,7 @@ router.post("/", async ctx => {
 });
 
 app
-  .use(cors({
-    origin: "*",
-    exposeHeaders: true,
-    allowHeaders: true
-  }))
+  .use(cors({ origin: "*" }))
   .use(bodyParser())
   .use(router.routes())
   .use(router.allowedMethods());
