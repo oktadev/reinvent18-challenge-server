79a02d85dd4c5fe8deb3110051dbb89aa656d3bc
Randall Degges <r@rdegges.com>
2018-11-20 23:40:29 -0800

    Testing.

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ router.post("/", async ctx => {
   if (created) {
     sendgrid.send({
       to: "r@rdegges.com",
-      from: "reinvent18-challenge@okta.com",
+      from: "r@rdegges.com",
       subject: "New Challenge Winner: " + json.email,
       text: "A new person has completed the challenge! Please give a prize to " + json.email + " when they come to the booth.",
       html: "A new person has completed the challenge! Please give a prize to " + json.email + " when they come to the booth."
